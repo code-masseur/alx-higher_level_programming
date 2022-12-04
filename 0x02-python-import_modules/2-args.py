6a3c5d7dd0e94fc0bf0920531741c0de03c002a5
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-03 17:28:07 -0800

    This program prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@
 if __name__ == "__main__":
 	from sys import argv
 
-if len(argv) == 1:
-    print('0 arguments.')
-elif len(argv) == 2:
-    print('{:d} argument:'.format(len(argv) - 1))
-else:
-    print('{:d} arguments:'.format(len(argv) - 1))
+    if len(argv) == 1:
+        print('0 arguments.')
+    elif len(argv) == 2:
+        print('{:d} argument:'.format(len(argv) - 1))
+    else:
+        print('{:d} arguments:'.format(len(argv) - 1))
 
-for i, arg in enumerate(argv[1:], start = 1):
-    print('{:d}: {:s}'.format(i, arg))
+    for i, arg in enumerate(argv[1:], start = 1):
+        print('{:d}: {:s}'.format(i, arg))
