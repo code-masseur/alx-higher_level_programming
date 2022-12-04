0744f858c408dcac660cfffa696072a671865bc5
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-03 18:07:49 -0800

    This program prints the result of the addition of all arguments to infinity

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    outcome = 0
+    for args in sys.argv:
+        if args != sys.argv[0]:
+            outcome += int(args)
+    else:
+        print(outcome) 
