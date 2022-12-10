f45d083addb95fe09e22c6b550064daf75a5fe01
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-09 18:11:43 -0800

    This function computes the square value of all integers of a matrix, returns the new matrix without modifying the original matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+def square_matrix_simple(matrix=[]):
+
+    if matrix is None:
+        return
+    else:
+        newList = []
+        for i in matrix:
+            modifiedList = list(map((lambda x: x ** 2), i))
+            newList.append(modifiedList)
+        return newList
