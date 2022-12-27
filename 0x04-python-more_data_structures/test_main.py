a67ca31bb619d5007a26240a455b1da13cccd4ae
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-26 16:49:42 -0800

    This function deletes a specified key from a dictionary

--- 0x04-python-more_data_structures/test_main.py
+++ 0x04-python-more_data_structures/test_main.py
@@ -2,6 +2,7 @@
 
 update_dictionary = __import__('7-update_dictionary').update_dictionary
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+simple_delete = __import__('8-simple_delete').simple_delete
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
@@ -24,5 +25,20 @@ new_dict = update_dictionary(a_dictionary, 'name', "Jude")
 print_sorted_dictionary(new_dict)
 print("--")
 print_sorted_dictionary(a_dictionary)
+new_dict = update_dictionary(a_dictionary, 'name', "Onyiagu")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+
+new_dict = simple_delete(a_dictionary, 'number')
+print_sorted_dictionary(new_dict)
+print("--")
+
+print_sorted_dictionary(a_dictionary)
+
+
+
 
 
