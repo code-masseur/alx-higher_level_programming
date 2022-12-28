65f8adefe6c62519e9138e583bc2e0a5a81a49ad
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-28 15:47:21 -0800

    This function  that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             pass
-        except (IndexError):
+        except IndexError:
             break
     print()
     return count
