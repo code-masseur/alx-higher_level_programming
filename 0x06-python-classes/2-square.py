d09b9f4a87ed78cf2b4fc7566df9373bf5f8594c
Jude Maduabuchi <github.icontrol@simplelogin.com>
2023-01-03 15:58:37 -0800

    This time i wrote the class to raise value or type exceptions if wrong value or type of data is passed as '__size'

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""This module defines a class 'Square'
+"""
+
+
+class Square:
+    """This is a simple 'Square' class.
+    Attribute:
+        size (int): the size of the 'Square'.
+    """
+    def __init__(self, size=0):
+        """Constructs a 'Square' object.
+        Args:
+            __size (int): the size of the 'Square'.
+                the default value is 0
+        Raises:
+            TypeError: if '__size' is not an integer.
+            ValueError: if '__size' is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif  size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
