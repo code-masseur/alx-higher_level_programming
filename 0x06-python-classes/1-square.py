e198a93d040112fdffdb9caaae8ad177d21f7f90
Jude Maduabuchi <github.icontrol@simplelogin.com>
2023-01-03 15:40:46 -0800

    This time, I instatiated the class with a private instance attribute of __size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module defines a class 'Square'
+"""
+
+
+class Square:
+    """This is a simple 'Square' class.
+    Attribute:
+        size (int): the size of the 'Square'.
+    """
+    def __init__(self, size):
+        """Constructs a 'Square' object.
+        Args:
+            __size (int): the size of the 'Square'.
+        """
+        self.__size = size
